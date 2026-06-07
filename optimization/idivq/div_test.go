a1cf3a4973abfa7c30ad58e6c36b14f908e16138
Molecular Man <a.berezh@gmail.com>
2026-06-07 07:29:02 +0200

    add inlined version of idivq benchmark

--- optimization/idivq/div_test.go
+++ optimization/idivq/div_test.go
@@ -3,6 +3,7 @@ package divbench
 import "testing"
 
 var strides = []int{4, 7, 13, 31, 64, 127, 200, 256}
+var divisors = [8]int{4, 7, 13, 31, 64, 127, 200, 256}
 
 var sink int
 
@@ -25,6 +26,27 @@ func BenchmarkDiv(b *testing.B) {
 		acc, i := 0, 0
 		for b.Loop() {
 			acc += divFloat(i&0xffff, strides[i&7])
+			i++
+		}
+		sink = acc
+	})
+}
+
+func BenchmarkDivInline(b *testing.B) {
+	b.Run("div=idivq", func(b *testing.B) {
+		acc, i := 0, 0
+		for b.Loop() {
+			stride := divisors[i&7]
+			acc += (256*(i&0xffff) + stride/2) / stride
+			i++
+		}
+		sink = acc
+	})
+	b.Run("div=float", func(b *testing.B) {
+		acc, i := 0, 0
+		for b.Loop() {
+			stride := divisors[i&7]
+			acc += int(float64(256*(i&0xffff)+stride/2) / float64(stride))
 			i++
 		}
 		sink = acc
