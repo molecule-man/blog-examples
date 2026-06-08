a80cdf1695e12de3175f8f5c8cc82873d39d1e6f
Molecular Man <a.berezh@gmail.com>
2026-06-08 16:14:01 +0200

    add benchmark for precalculated reciprocal

--- optimization/idivq/div_test.go
+++ optimization/idivq/div_test.go
@@ -52,3 +52,58 @@ func BenchmarkDivInline(b *testing.B) {
 		sink = acc
 	})
 }
+
+var fnum [256]float64
+var fden [256]float64
+var fsink float64
+
+func init() {
+	x := uint64(12345)
+	for i := range fnum {
+		x = x*6364136223846793005 + 1442695040888963407 // LCG, runtime values
+		fnum[i] = float64((x >> 40) & ((1 << 24) - 1))
+		fden[i] = float64(4 + (x>>8)&255)
+	}
+}
+
+func BenchmarkDivThroughput(b *testing.B) {
+	acc, i := 0.0, 0
+	for b.Loop() {
+		acc += fnum[i&255] / fden[i&255]
+		i++
+	}
+	fsink = acc
+}
+
+const reciprocalShift = 40
+
+func BenchmarkDivSameDivisor(b *testing.B) {
+	stride := divisors[3] // 31, a runtime value
+	recip := (uint64(1)<<reciprocalShift + uint64(stride) - 1) / uint64(stride)
+
+	b.Run("div=idivq", func(b *testing.B) {
+		acc, i := 0, 0
+		for b.Loop() {
+			acc += (256*(i&0xffff) + stride/2) / stride
+			i++
+		}
+		sink = acc
+	})
+	b.Run("div=float", func(b *testing.B) {
+		acc, i := 0, 0
+		for b.Loop() {
+			acc += int(float64(256*(i&0xffff)+stride/2) / float64(stride))
+			i++
+		}
+		sink = acc
+	})
+	// Correctness wasn't verified. Careful
+	b.Run("div=reciprocal", func(b *testing.B) {
+		acc, i := 0, 0
+		for b.Loop() {
+			acc += int((uint64(256*(i&0xffff)+stride/2) * recip) >> reciprocalShift)
+			i++
+		}
+		sink = acc
+	})
+}
